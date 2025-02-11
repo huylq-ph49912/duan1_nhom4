e6221dad8ac79c77201673c86345162e7229e1e1
nguyen duc duong <Nguyen Duc Duong>
2025-02-11 23:30:09 +0700

    duong lam giao dien admin

---
+++ admin/views/layout/sidebar.php
@@ -0,0 +1,39 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <div style="text-align:center;">
+    <a href="#" class="brand-link">
+      <span class="brand-text font-weight-light" style="font-size:35px; color: white;">Mixi <span
+          style="color: red;">Shop</span></span>
+    </a>
+  </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="./assets/dist/img/mixi.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block" style="font-size:15px; color: white;">MixiGaming</a>
+      </div>
+    </div>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="<?= BASE_URL_ADMIN ?>" class="nav-link">
+            <i class="nav-icon fas fa-home"></i>
+            <p style="color: white;">
+              Trang Chủ
+            </p>
+          </a>
+        </li>
+        
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
